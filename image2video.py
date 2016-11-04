65cf460bcb69156d25b9caffc637936b5859c87e
Lingxue Zhu <lingxue.zhu@gmail.com>
2016-11-04 17:25:59 -0400

    A small test video; style transferred frame-by-frame

---
+++ image2video.py
@@ -0,0 +1,49 @@
+from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+import os, exceptions, re
+
+def image2video(images, outvid=None, fps=5, size=None,
+               is_color=True, format="mp4v"):
+    """
+    Create a video from a list of images.
+ 
+    @param      outvid      output video
+    @param      images      list of images to use in the video
+    @param      fps         frame per second
+    @param      size        size of each frame
+    @param      is_color    color
+    @param      format      see http://www.fourcc.org/codecs.php
+    @return                 see http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+ 
+    The function relies on http://opencv-python-tutroals.readthedocs.org/en/latest/.
+    By default, the video will have the size of the first image.
+    It will resize every image to this size before adding them to the video.
+    """
+    fourcc = VideoWriter_fourcc(*format)
+    vid = None
+    for image in images:
+        if not os.path.exists(image):
+            raise IOError(image)
+        img = imread(image)
+        if vid is None:
+            if size is None:
+                size = img.shape[1], img.shape[0]
+            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
+        if size[0] != img.shape[1] and size[1] != img.shape[0]:
+            img = resize(img, size)
+        vid.write(img)
+
+    vid.release()
+    return vid
+
+
+if __name__ == "__main__":
+    ## !!!WARNING!!!
+    ## the only digits in the filename must be the frame number
+    imageDir = "frames_styled/"
+    images = [imageDir + imageName for imageName in os.listdir(imageDir)]
+
+    ## sort image files according to frame number
+    images.sort(key=lambda name: int(re.sub("\D", "", name)))
+
+    ## convert to video
+    image2video(images, outvid="styled_test.mp4")
---
+++ video2image.py
@@ -0,0 +1,18 @@
+import cv2
+
+def video2image(videoFile, outdir, extractGap, outprefix="frame"):
+
+	vidcap = cv2.VideoCapture(videoFile)
+	success,image = vidcap.read()
+	count = 0
+	success = True
+	while success:
+		success,image = vidcap.read()
+		if count % extractGap == 0:
+			print 'Read a new frame: ', success
+			cv2.imwrite(("%s%s%d.jpg" % (outdir, outprefix, count)), image) 
+		count += 1
+
+
+if __name__ == "__main__":
+	video2image(videoFile='test.MOV', outdir="frames/", extractGap=10)
