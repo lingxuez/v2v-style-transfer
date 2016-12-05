114b0bdb0a1d6bf43f0dea16f295671abab6febd
Lingxue Zhu <lingxue.zhu@gmail.com>
2016-12-05 16:20:44 -0500

    test merging 2 styles

--- multipleStyleTransfer.sh
+++ multipleStyleTransfer.sh
@@ -1,12 +1,15 @@
-## Usage: sh rawTransferVideo.sh
+## Usage: sh multipleStyleTransfer.sh
 ## Note: need to download neural-style code first:
 ## git clone git@github.com:lingxuez/neural-style.git
 
+cd neural-style
+
 style_dir="../video2video/trailer/frames_lof"
+content="../data/frames/frame0.jpg"
+output="../video2video/stylized/frame0_2style.jpg"
 
 ## try 1 frame, 2 styles
-cd neural-style
-th neural_style.lua -content_image ../data/frames/frame0.jpg \
+th neural_style.lua -content_image $content \
 	-style_image ${style_dir}/frame-01.jpeg,${style_dir}/frame-02.jpeg \
-	-gpu -1 ## cpu mode
-	
+	-output_image $output \
+	-num_iterations 200 -gpu -1 ## cpu mode
