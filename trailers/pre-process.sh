74ee0b2f4f8fa6f78f78918f7183b4f769affb2f
Lingxue Zhu <lingxue.zhu@gmail.com>
2016-12-07 22:46:41 -0500

    clean up pre-process for keyframes

--- trailers/pre-process.sh
+++ trailers/pre-process.sh
@@ -20,17 +20,18 @@
 ## Installation:
 ## $ brew install ffmpeg
 
-outdir="frames_bigfish" ## output folder
+video="bigfish/bigfish.mp4"
+outdir="bigfish/frames_bigfish" ## output folder
 digits="%03d" ## pad zeros before frame numbers to make it 3 digits
 prefix="frame" ## prefix for extracted frames
 
 ## extract key frames
-ffmpeg -i bigfish.mp4 -vf select='eq(pict_type\,I)' \
+ffmpeg -i $video -vf select='eq(pict_type\,I)' \
 	 -vsync 2 -f image2 ${outdir}/${prefix}${digits}.jpg
 
 ## original indices of key frames
 ffprobe -select_streams v -show_frames -show_entries frame=pict_type \
-	-of csv bigfish.mp4 \
+	-of csv $video \
 	| grep -n "frame,I" | cut -d ":" -f 1 > ${outdir}/index.txt 
 
 ## re-name key frames using their original indices
@@ -43,3 +44,6 @@ do
 	newIndex=$(printf ${digits} $(echo $index - 1 | bc))
 	mv ${old_name} ${prefix}${newIndex}.jpg
 done 
+
+## remove temporary files
+rm *.txt
